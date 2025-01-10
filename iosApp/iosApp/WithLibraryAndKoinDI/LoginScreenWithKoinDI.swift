9120ea72714ad0e08efab65c9893e00132d21701
ankushkushwaha <ankush7377@gmail.com>
2025-01-10 11:36:01 +0530

    Add login screen for Koin POC

--- iosApp/iosApp/WithLibraryAndKoinDI/LoginScreenWithKoinDI.swift
+++ iosApp/iosApp/WithLibraryAndKoinDI/LoginScreenWithKoinDI.swift
@@ -6,4 +6,89 @@
 //  Copyright © 2025 orgName. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import KMPObservableViewModelSwiftUI
+import Shared
+
+struct LoginScreenWithKoinDI: View {
+    @StateViewModel var viewModel = LoginSharedViewModelLib()
+
+    @State private var username: String = ""
+    @State private var password: String = ""
+
+    @State private var showPassword: Bool = false
+    
+    @State private var showDetail: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 20) {
+                
+                TextField("Email", text: $username)
+                    .textContentType(.emailAddress)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(8)
+                
+                HStack {
+                    if showPassword {
+                        TextField("Password", text: $password)
+                    } else {
+                        SecureField("Password", text: $password)
+                    }
+                    Button(action: {
+                        showPassword.toggle()
+                    }) {
+                        Image(systemName: showPassword ? "eye.slash" : "eye")
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding()
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(8)
+                
+                // Login Button
+                Button(action: viewModel.startFetch) {
+                    if viewModel.viewStateValue.isLoading?.boolValue ?? false {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                    } else {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                    }
+                }
+                .disabled(!(viewModel.viewStateValue.isValidCredentials?.boolValue ?? false))
+                .opacity(viewModel.viewStateValue.isValidCredentials?.boolValue ?? false ? 1 : 0.5)
+                
+                ChildView(name: $username)
+            }
+            .padding()
+            
+            .navigationDestination(isPresented: $showDetail) {
+                ContentView()
+            }
+
+            .onChange(of: username) { _ in
+                print("updateViewModel username")
+                viewModel.setUsername(usernme: username)
+            }
+            .onChange(of: password) { _ in
+                print("updateViewModel password")
+               viewModel.setPassword(password: password)
+            }
+            .onChange(of: showDetail) { _ in
+                print("updateViewModel showDetail")
+                viewModel.setPushToHomeScreen(pushToHomeScreen: showDetail)
+            }
+            .onChange(of: viewModel.viewStateValue.pushToHomeScreen) { _ in
+                showDetail = viewModel.viewStateValue.pushToHomeScreen?.boolValue ?? false
+            }
+        }
+    }
+}
